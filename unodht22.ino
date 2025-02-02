983590d7097c1b5a2a73877d345f22b12d070360
sg-hk <rubenruben000@proton.me>
2025-02-02 17:27:56 +0800

    initial commit

---
+++ unodht22.ino
@@ -0,0 +1,28 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 2        // Pin where DHT22 is connected
+#define DHTTYPE DHT22   // Define sensor type (DHT22)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+        Serial.begin(9600);
+        dht.begin();
+}
+
+void loop() {
+        float temp = dht.readTemperature();
+        float hum = dht.readHumidity();  
+
+        if (isnan(temp) || isnan(hum)) {
+                Serial.println("Failed to read from DHT sensor"); 
+        } else {
+                Serial.print(temp);
+                Serial.print(",");
+                Serial.println(hum);
+        }
+
+        delay(60000); // read every minute
+}
