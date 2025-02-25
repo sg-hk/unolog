ea9e2b39c7c7207b9e02fe14dfaf892eda88b6d2
sg-hk <rubenruben000@proton.me>
2025-02-25 16:08:15 +0800

    fix float to string

--- unodht22.ino
+++ unodht22.ino
@@ -5,6 +5,7 @@
 #define DHTPIN 2      
 #define DHTTYPE DHT22 
 #define SERIAL_BAUD 9600
+#define REFRESH_RATE 2000 // 2s
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -14,16 +15,18 @@ void setup() {
 }
 
 void loop() {
-  float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
 
-  if (!isnan(humidity) && !isnan(temperature)) {
-    /* first build the string, then output it
-     * this prevents partial reads from Serial.print()s */
-    char output[16];  
-    snprintf(output, sizeof(output), "%.1fC %.1f%%", temperature, humidity);
-    Serial.println(output);
-  }
+  if (isnan(humidity) || isnan(temperature))
+    goto sleep;
 
-  delay(60000); // 1m
+  char temp_str[8], hum_str[8], output[32];  
+  dtostrf(temperature, 4, 1, temp_str); // snprintf %f directly doesn't work
+  dtostrf(humidity, 4, 1, hum_str);  
+  snprintf(output, sizeof(output), "%sC %s%%\n", temp_str, hum_str);
+  Serial.print(output);
+
+sleep:
+  delay(REFRESH_RATE);
 }
